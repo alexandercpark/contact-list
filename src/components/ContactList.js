0ab595f2d53ab51fa7b2552854884784c361e0ab
Alexander Park <alexandercpark@gmail.com>
2018-08-23 14:14:11 -0400

    adding more mock data

--- src/components/ContactList.js
+++ src/components/ContactList.js
@@ -13,7 +13,10 @@ class ContactList extends Component {
       this.props.contacts.map( (contact, index) => (
         <div className="contact">
           <img src={contact.image_url} style={{height: 100}} />
-          {contact.name}<a href="#">edit</a> <a href="#">delete</a>
+          {contact.name}
+          <a href={"mailto:" + contact.email}>email</a>
+          <a href={"tel:"+ contact.phone_number}>call</a>
+          <a href="#">edit</a> <a href="#">delete</a>
         </div>
       ))
     }
