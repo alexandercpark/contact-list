bcfa737fd063c1b07d3a8cc0277ec9ef11b06c32
Alexander Park <alexandercpark@gmail.com>
2018-08-23 13:43:02 -0400

    creating mockup

--- src/components/App.js
+++ src/components/App.js
@@ -6,13 +6,14 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className="heading">
+          Contacts <button type="button">Add Contact</button>
+        </div>
+        <div className="contactList">
+        <div className="contact">Albert Einstein <a href="#">edit</a> <a href="#">delete</a></div>
+        <div className="contact">Neils Bohr <a href="#">edit</a> <a href="#">delete</a></div>
+        <div className="contact">Ryan Gosling <a href="#">edit</a> <a href="#">delete</a></div>
+        </div>
       </div>
     );
   }
