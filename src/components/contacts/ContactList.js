76f51802c9e17102c837d043ed145596f4848296
Alexander Park <alexandercpark@gmail.com>
2018-08-26 17:28:34 -0400

    styling

--- src/components/contacts/ContactList.js
+++ src/components/contacts/ContactList.js
@@ -7,16 +7,20 @@ class ContactList extends Component {
   }
 
   render = () => (
-    <div className="contactList">
-      {
-        this.props.contacts.map(contact => (
-            <span className="contact-container" key={contact.id}>
-              <Contact contact={contact} deleteContact={this.props.deleteContact}/>
-            </span>
+    <div>
+      <div className="contactList">
+        {
+          this.props.contacts.map(contact => (
+              <span className="contact-container" key={contact.id}>
+                <Contact contact={contact} deleteContact={this.props.deleteContact}/>
+              </span>
+            )
           )
-        )
-      }
-      <button className="btn btn-primary" onClick={this.props.loadContactsFromGoogle}>Load contacts from Google</button>
+        }
+      </div>
+      <div>
+        <button className="btn btn-primary" onClick={this.props.loadContactsFromGoogle}>Load contacts from Google</button>
+      </div>
     </div>
   )
 
